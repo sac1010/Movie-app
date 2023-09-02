b63d98999b65d3c4a22984c05159917be402181b
sac1010 <sachingirish101@gmail.com>
2023-09-02 09:47:18 +0530

    added link

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -2,31 +2,36 @@ import React, { useContext, useState } from "react";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 import { SearchContext } from "../context";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const Navbar = () => {
-  const router = useRouter()
+  const router = useRouter();
   const [search, setSearch] = useContext(SearchContext);
   const [val, setVal] = useState("");
   const handleSearch = () => {
     if (val.trim() === "") {
       return;
     }
-    setSearch(val)
-    router.push("/")
+    setSearch(val);
+    router.push("/");
   };
 
   return (
     <div className="w-full h-[5rem] bg-white dark:bg-black shadow-xl border-b border-gray-500 flex justify-around items-center">
-      <div className="text-black dark:text-white font-extrabold text-xl hidden md:block">
+      <Link
+        href={"/"}
+        className="text-black dark:text-white font-extrabold text-xl hidden md:block"
+      >
         Wookie Movies
-      </div>
+      </Link>
       <div className="flex gap-6">
         <input
           onChange={(e) => {
-            if(e.target.value.trim()===""){
-              setSearch("")
+            if (e.target.value.trim() === "") {
+              setSearch("");
             }
-            setVal(e.target.value)}}
+            setVal(e.target.value);
+          }}
           type="text"
           className="px-2 border border-black"
           placeholder="Search"
